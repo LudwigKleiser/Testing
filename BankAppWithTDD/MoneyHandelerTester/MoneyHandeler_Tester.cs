9044dcb4ba3f0a9fea4a077a22c158cc198543af
ludde <ludde@DESKTOP-BOCQCTT>
2016-12-22 15:42:05 +0100

    UnitTesting!

---
+++ BankAppWithTDD/MoneyHandelerTester/MoneyHandeler_Tester.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneyHandeler_TDD;
+using NUnit.Framework;
+namespace MoneyHandelerTester
+{
+    [TestFixture]
+    public class MoneyHandeler_Tester
+    {
+        [Test]
+        public void DebitFromBalance_Test()
+        {
+
+            int expectedBalance = 250;
+            int balanceToRemove = 250;
+            MoneyHandeler moneyHandeler = new MoneyHandeler();
+            int actualBalance = moneyHandeler.Debit(balanceToRemove);
+
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+
+        [Test]
+
+        public void CreditToBalance_Test()
+        {
+
+            int expectedBalance = 5000;
+            int balanceToAdd = 4500;
+            MoneyHandeler moneyHandeler = new MoneyHandeler();
+            int actualBalance = moneyHandeler.Credit(balanceToAdd);
+
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+
+        [Test]
+
+        public void PrintBalance_Test()
+        {
+
+            int expectedOutput = 500;
+
+            MoneyHandeler moneyHandeler = new MoneyHandeler();
+            int actualBalance = moneyHandeler.PrintBalance(moneyHandeler.Balance);
+
+            Assert.AreEqual(expectedOutput, actualBalance);
+        }
+
+        [Test]
+
+        public void Removing_600_From_500_With_No_Change_Test()
+        {
+            int balanceToRemove = 600;
+            int expectedBalance = 500;
+
+            MoneyHandeler moneyHandeler = new MoneyHandeler();
+
+            int actualBalance = moneyHandeler.Debit(balanceToRemove);
+
+            Assert.AreEqual(expectedBalance, actualBalance);
+
+        }
+
+        [Test]
+
+        public void BalanceOver_50000_Test()
+        {
+            int expectedBalance = 500;
+            int balanceToAdd = 49501;
+
+            MoneyHandeler moneyHandeler = new MoneyHandeler();
+
+            int actualBalance = moneyHandeler.Credit(balanceToAdd);
+
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+
+        [Test]
+
+        public void CreditWithANegativeNumber()
+        {
+            int expectedBalance = 500;
+            int balanceToAdd = -500;
+
+            MoneyHandeler moneyHandeler = new MoneyHandeler();
+
+            int actualBalance = moneyHandeler.Credit(balanceToAdd);
+
+
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+
+        [Test]
+
+        public void MaxCredit30000()
+        {
+            int expectedBalance = 500;
+            int balanceToAdd = 30000;
+
+            MoneyHandeler moneyHandeler = new MoneyHandeler();
+
+            int actualBalance = moneyHandeler.Credit(balanceToAdd);
+
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+
+        [Test]
+
+        public void MinimumCreditTest([Values(25, 45, 38, 99, 48)]int input)
+        {
+            MoneyHandeler moneyHandeler = new MoneyHandeler();
+            int expectedBalance = moneyHandeler.Balance;
+
+            Assert.AreEqual(expectedBalance, moneyHandeler.Credit(input));
+
+        }
+
+       
+        
+       
+            
+    }
+}
---
+++ BankAppWithTDD/MoneyHandeler_TDD/MoneyHandeler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyHandeler_TDD
+{
+    
+    public class MoneyHandeler
+    {
+        public int Balance { get; set; } = 500;
+        public int Debit(int balanceToRemove)
+        {
+            if (balanceToRemove > Balance) return Balance;
+
+            return Balance - balanceToRemove ;
+
+            
+        }
+
+        public int Credit(int balanceToAdd)
+        {
+            if (balanceToAdd < 100) return Balance;
+            if (balanceToAdd <= 0) return Balance;
+            if (balanceToAdd + Balance > 50000) return Balance;
+            if (balanceToAdd >= 30000) return Balance;
+
+            return balanceToAdd + Balance;
+        }
+
+        public int PrintBalance(int balanceToPrint)
+        {
+            return balanceToPrint;
+        }
+    }
+}
